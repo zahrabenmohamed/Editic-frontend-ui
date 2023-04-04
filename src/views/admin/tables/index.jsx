7b08eeeccfa4c34b9615f2ddaae9ce5470f8deed
zahrabenmohamed <benmedzahra20@gmail.com>
2023-04-04 22:01:01 +0000

    first commit

---
+++ src/views/admin/tables/index.jsx
@@ -0,0 +1,26 @@
+
+import {
+  
+  columnsDataComplex,
+} from "./variables/columnsData";
+import tableDataComplex from "./variables/tableDataComplex.json";
+import ComplexTable from "./components/ComplexTable";
+
+const Tables = () => {
+  return (
+    <div>
+
+
+      <div className="mt-5 grid h-full grid-cols-1 gap-5 md:grid-cols-2">
+        
+
+        <ComplexTable
+          columnsData={columnsDataComplex}
+          tableData={tableDataComplex}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Tables;
