04b21891ef4ed5d8f2641f4b8e983dd545220e6d
zahrabenmohamed <benmedzahra20@gmail.com>
2023-04-06 12:22:17 +0000

    clean code v3.1

--- src/components/footer/FooterAuthDefault.jsx
+++ src/components/footer/FooterAuthDefault.jsx
@@ -19,15 +19,6 @@ export default function Footer() {
         <li className="mr-12">
           <a
             target="blank"
-            href="https://simmmple.com/licenses"
-            className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
-          >
-            License
-          </a>
-        </li>
-        <li className="mr-12">
-          <a
-            target="blank"
             href="https://simmmple.com/terms-of-service"
             className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
           >
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -81,34 +81,6 @@ const Navbar = (props) => {
                   Mark all read
                 </p>
               </div>
-
-              <button className="flex w-full items-center">
-                <div className="flex h-full w-[85px] items-center justify-center rounded-xl bg-gradient-to-b from-brandLinear to-brand-500 py-4 text-2xl text-white">
-                  <BsArrowBarUp />
-                </div>
-                <div className="ml-2 flex h-full w-full flex-col justify-center rounded-lg px-1 text-sm">
-                  <p className="mb-1 text-left text-base font-bold text-gray-900 dark:text-white">
-                    New Update: Horizon UI Dashboard PRO
-                  </p>
-                  <p className="font-base text-left text-xs text-gray-900 dark:text-white">
-                    A new update for your downloaded item is available!
-                  </p>
-                </div>
-              </button>
-
-              <button className="flex w-full items-center">
-                <div className="flex h-full w-[85px] items-center justify-center rounded-xl bg-gradient-to-b from-brandLinear to-brand-500 py-4 text-2xl text-white">
-                  <BsArrowBarUp />
-                </div>
-                <div className="ml-2 flex h-full w-full flex-col justify-center rounded-lg px-1 text-sm">
-                  <p className="mb-1 text-left text-base font-bold text-gray-900 dark:text-white">
-                    New Update: Horizon UI Dashboard PRO
-                  </p>
-                  <p className="font-base text-left text-xs text-gray-900 dark:text-white">
-                    A new update for your downloaded item is available!
-                  </p>
-                </div>
-              </button>
             </div>
           }
           classNames={"py-2 top-4 -left-[230px] md:-left-[440px] w-max"}
